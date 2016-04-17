0482d23295aaa9e5a939afc24ecfa9a057ee4821
DerGenaue <dergenaue@online.de>
2016-04-17 18:22:41 +0200

    Basics: Chat, Items

---
+++ WarframeMarketClient/GUI/Tabs/Chat.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WarframeMarketClient.GUI.Tabs
+{
+    /// <summary>
+    /// Interaktionslogik für Chat.xaml
+    /// </summary>
+    public partial class Chat : UserControl
+    {
+        public Chat()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return){
+                if((e.KeyboardDevice.Modifiers & (ModifierKeys.Shift | ModifierKeys.Control)) != 0) {
+                    int caretIndex = InputText.CaretIndex;
+                    InputText.Text = InputText.Text.Insert(InputText.CaretIndex, "\r\n");
+                    InputText.CaretIndex = caretIndex+1;
+                }
+            }
+        }
+    }
+}
---
+++ WarframeMarketClient/GUI/Tabs/Tab_Items.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WarframeMarketClient.GUI.Tabs
+{
+    /// <summary>
+    /// Interaktionslogik für Tab_Items.xaml
+    /// </summary>
+    public partial class Tab_Items : UserControl
+    {
+        public Tab_Items()
+        {
+            InitializeComponent();
+
+            List<Item> items = new List<Item>();
+            items.Add(new Item() { Category = "Bla", Name = "Blablub", Price=20, Count=2, Rank=0, Buttons="Nix" });
+            items.Add(new Item() { Category = "Bla", Name = "Blablub", Price = 20, Count = 2, Rank = 0, Buttons = "Nix" });
+            items.Add(new Item() { Category = "Bla", Name = "Blablub", Price = 20, Count = 2, Rank = 0, Buttons = "Nix" });
+            //list.ItemsSource = items;
+        }
+    }
+
+    public class Item
+    {
+        public string Category { get; set; }
+
+        public string Name { get; set; }
+
+        public int Price { get; set; }
+
+        public int Count { get; set; }
+
+        public int Rank { get; set; }
+
+        public string Buttons { get; set; }
+    }
+
+}
