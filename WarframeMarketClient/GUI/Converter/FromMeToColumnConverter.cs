dad1b5b34bd8476f8c4d36e935b00b83ae8a4b0f
DerGenaue <dergenaue@online.de>
2016-05-14 03:53:02 +0200

    Chat alignment improved

---
+++ WarframeMarketClient/GUI/Converter/FromMeToColumnConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WarframeMarketClient.GUI.Converter
+{
+    class FromMeToColumnConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool? IsFromMe = value as bool?;
+            if (IsFromMe != null && IsFromMe == true)
+                return 2;
+            return 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
