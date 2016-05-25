7793a423cebce9a0785dd6e12914b6f364bace47
DerGenaue <dergenaue@online.de>
2016-05-26 01:35:44 +0200

    Modrank not editable when no mpdrank available Improving TabLayout alignment (Remove visual artifacts)

---
+++ WarframeMarketClient/GUI/Converter/ModRankToBooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WarframeMarketClient.GUI.Converter
+{
+    class ModRankToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value as int?) < 0 ? false : true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
