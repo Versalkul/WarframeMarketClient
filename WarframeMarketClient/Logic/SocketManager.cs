bfbf7d2b19cff02c48d6a2bfca47d97d5bef61d6
Versalkul <xarlas@yahoo.de>
2018-08-22 22:50:32 +0200

    Fix websockets part 1

--- WarframeMarketClient/Logic/SocketManager.cs
+++ WarframeMarketClient/Logic/SocketManager.cs
@@ -3,10 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using WarframeMarketClient.Extensions;
 using WarframeMarketClient.Model;
-using WebSocket4Net;
+using WebSocketSharp;
 
 namespace WarframeMarketClient.Logic
 {
@@ -21,19 +22,22 @@ namespace WarframeMarketClient.Logic
 
         private Queue<string> jsonsToSend = new Queue<string>(5);
 
-        public SocketManager():this(ApplicationState.getInstance().SessionToken)
+        public SocketManager() : this(ApplicationState.getInstance().SessionToken)
         {
         }
         public SocketManager(string cookie)
         {
-            List<KeyValuePair<string, string>> cookies = new List<KeyValuePair<string, string>>(1);
-            cookies.Add(new KeyValuePair<string, string>("session", cookie));
-            socket = new WebSocket("wss://warframe.market/socket", "", cookies);
-            socket.Opened += new EventHandler(onOpen);
-            socket.Error += new EventHandler<SuperSocket.ClientEngine.ErrorEventArgs>(onError);
-            socket.MessageReceived += new EventHandler<MessageReceivedEventArgs>(onMessage);
-            socket.Closed += Socket_Closed;
-            socket.EnableAutoSendPing = false;
+
+            socket = new WebSocket("wss://warframe.market/socket");
+            socket.SslConfiguration.EnabledSslProtocols = System.Security.Authentication.SslProtocols.Tls12;
+
+            socket.SetCookie(new WebSocketSharp.Net.Cookie("JWT", cookie));
+            socket.OnOpen += new EventHandler(onOpen);
+            socket.OnError += new EventHandler<WebSocketSharp.ErrorEventArgs>(onError);
+            socket.OnMessage += new EventHandler<MessageEventArgs>(onMessage);
+            socket.OnClose += Socket_Closed;
+            socket.Connect();
+            Console.WriteLine("Open");
         }
 
         #region events
@@ -41,7 +45,7 @@ namespace WarframeMarketClient.Logic
         private void Socket_Closed(object sender, EventArgs e)
         {
 
-            ApplicationState.getInstance().Logger.Log("CLOSED SOCKET ",false);
+            ApplicationState.getInstance().Logger.Log("CLOSED SOCKET ", false);
             SocketWasClosed = true;
             if (ApplicationState.getInstance().OnlineState.IsOnline())
             {
@@ -53,39 +57,39 @@ namespace WarframeMarketClient.Logic
         }
 
 
-        private void onError(object caller, SuperSocket.ClientEngine.ErrorEventArgs args)
+        private void onError(object caller, ErrorEventArgs args)
         {
-            ApplicationState.getInstance().Logger.Log("Error from websocket:",false);
-            ApplicationState.getInstance().Logger.Log(args.Exception.Message,false);
-            
+            ApplicationState.getInstance().Logger.Log("Error from websocket:", false);
+            ApplicationState.getInstance().Logger.Log(args.Exception.Message, false);
+
 
         }
 
-        public void onMessage(object caller, MessageReceivedEventArgs args)
+        public void onMessage(object caller, MessageEventArgs args)
         {
             ApplicationState.getInstance().Logger.Log("Got Data from websocket", false);
-            if (recievedPM != null && args.Message.Contains("recive_message"))
+            if (recievedPM != null && args.Data.Contains("recive_message"))
             {
-                SocketMessage msg = JsonConvert.DeserializeObject<SocketMessage>(args.Message);
+                SocketMessage msg = JsonConvert.DeserializeObject<SocketMessage>(args.Data);
                 recievedPM.Invoke(this, new PmArgs(msg.data.text, msg.data.from));
             }
-            Console.WriteLine(args.Message);
+            Console.WriteLine(args.Data);
         }
 
 
         private void onOpen(object sender, EventArgs args)
         {
-            ApplicationState.getInstance().Logger.Log("Socket open",false);
+            ApplicationState.getInstance().Logger.Log("Socket open", false);
             SocketWasClosed = true;
             lock (socket)
             {
 
-            if (jsonsToSend.Count != 0)
-                while (jsonsToSend.Count > 0)
-                    socket.Send(jsonsToSend.Dequeue());
+                if (jsonsToSend.Count != 0)
+                    while (jsonsToSend.Count > 0)
+                        socket.Send(jsonsToSend.Dequeue());
                 return; //Remove
-            if (ApplicationState.getInstance().OnlineState == OnlineState.OFFLINE|| ApplicationState.getInstance().OnlineState==OnlineState.DISABLED)
-            {
+                if (ApplicationState.getInstance().OnlineState == OnlineState.OFFLINE || ApplicationState.getInstance().OnlineState == OnlineState.DISABLED)
+                {
                     ApplicationState.getInstance().Logger.Log("CLOSING SOCKET after opening", false);
                     socket.Close();
                 }
@@ -106,32 +110,32 @@ namespace WarframeMarketClient.Logic
         {
             lock (socket)
             {
-                if (socket.State == WebSocketState.Closed||socket.State==WebSocketState.None) socket.Open();
+                if (!socket.IsAlive) socket.Connect();
             }
         }
 
         private void sendJson(string json)
         {
-            ApplicationState.getInstance().Logger.Log("Sending json",false);
+            ApplicationState.getInstance().Logger.Log("Sending json", false);
             lock (socket)
             {
-                if (socket.State == WebSocketState.Closed || socket.State == WebSocketState.None|| socket.State == WebSocketState.Connecting)
+                if (!socket.IsAlive)
                 {
                     jsonsToSend.Enqueue(json);
-                    if(socket.State != WebSocketState.Connecting) socket.Open();
+                    socket.Connect();
                 }
                 else
                 {
                     socket.Send(json);
                 }
-                    
+
             }
         }
 
         private void sendIngameJson()
         {
-            ApplicationState.getInstance().Logger.Log("Trying to go ingame",false);
-            sendJson("{\"destination\":\"user.set_status\",\"data\":{\"status\":\"in_game\"}}");
+            ApplicationState.getInstance().Logger.Log("Trying to go ingame", false);
+            sendJson("{\"type\": \"@WS/USER/SET_STATUS\", \"payload\": \"ingame\"}");
         }
 
         public void setIngame()
@@ -141,19 +145,14 @@ namespace WarframeMarketClient.Logic
 
         public void setOnline()
         {
-            ApplicationState.getInstance().Logger.Log("Trying to go online",false);
-            sendJson("{\"destination\":\"user.set_status\",\"data\":{\"status\":\"online\"}}");
+            ApplicationState.getInstance().Logger.Log("Trying to go online", false);
+            sendJson("{\"type\": \"@WS/USER/SET_STATUS\", \"payload\": \"online\"}");
         }
 
         public void setOffline()
-        { 
-            sendJson("{\"destination\":\"user.set_status\",\"data\":{\"status\":\"online\"}}");
-            lock (socket)
-            {
-                if (socket.State == WebSocketState.Open && jsonsToSend.Count == 0)
-                    socket.Close();
-            }
-            ApplicationState.getInstance().Logger.Log("CLOSING SOCKET GOING OFFLINE",false);
+        {
+            sendJson("{\"type\": \"@WS/USER/SET_STATUS\", \"payload\": \"invisible\"}");
+            ApplicationState.getInstance().Logger.Log("GOING OFFLINE", false);
         }
 
         public void Dispose()
@@ -162,12 +161,12 @@ namespace WarframeMarketClient.Logic
             if (disposed) return;
             disposed = true;
             setOffline();
-            socket.Dispose();
+            socket.Close();
         }
 
         ~SocketManager()
         {
-           if(!disposed) Dispose();
+            if (!disposed) Dispose();
         }
     }
 }
